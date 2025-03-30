aff94c28868be9a45346850717b9ad0360fc0a2c
Moaksh Kakkar <moakshkakar@gmail.com>
2025-03-30 16:22:33 +0530

    spelling mistake fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 export const metadata: Metadata = {
-  title: "Moaksh Kakar",
+  title: "Moaksh KakKar",
   description:
     "Hi, I am Moaksh Kakkar, a dedicated individual with a relentless passion for learning and growth. Driven by curiosity and a thirst for knowledge, I approach every opportunity with enthusiasm and a commitment to excellence. With a solid foundation in programming languages and cutting-edge technologies, I thrive in dynamic environments where I can apply my skills to solve complex problems and drive innovation. My goal is to continuously expand my horizons, embrace challenges, and make a positive impact in the world of technology.",
 };
