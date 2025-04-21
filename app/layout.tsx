6710e90281bba210d9c8fb4bf7e845ecf0f462f9
Moaksh Kakkar <moakshkakar@gmail.com>
2025-04-22 01:19:37 +0530

    docs(layout): fix typo in title and add SEO keywords
    Correct the typo in the title from "Moaksh KakKar" to "Moaksh Kakkar" and add a comprehensive list of SEO keywords to improve search engine visibility for the portfolio website.

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,43 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 export const metadata: Metadata = {
-  title: "Moaksh KakKar",
+  title: "Moaksh Kakkar",
   description:
     "Hi, I am Moaksh Kakkar, a dedicated individual with a relentless passion for learning and growth. Driven by curiosity and a thirst for knowledge, I approach every opportunity with enthusiasm and a commitment to excellence. With a solid foundation in programming languages and cutting-edge technologies, I thrive in dynamic environments where I can apply my skills to solve complex problems and drive innovation. My goal is to continuously expand my horizons, embrace challenges, and make a positive impact in the world of technology.",
+  keywords: [
+    "Moaksh Kakkar",
+    "Moaksh",
+    "Kakkar",
+    "Moaksh Kakkar Portfolio",
+    "Moaksh Kakkar Resume",
+    "Moaksh Kakkar Blog",
+    "Moaksh Kakkar LinkedIn",
+    "Moaksh Kakkar GitHub",
+    "Moaksh Kakkar Twitter",
+    "Moaksh Kakkar Instagram",
+    "Moaksh Kakkar Facebook",
+    "Moaksh Kakkar YouTube",
+    "Moaksh Kakkar TikTok",
+    "Moaksh Kakkar Medium",
+    "Moaksh Kakkar HackerRank",
+    "Moaksh Kakkar LeetCode",
+    "Moaksh Kakkar CodeChef",
+    "Moaksh Kakkar Codeforces",
+    "Moaksh Kakkar HackerEarth",
+    "Moaksh Kakkar GeeksForGeeks",
+    "Moaksh Kakkar TopCoder",
+    "Moaksh Kakkar HackerRank",
+    "Moaksh Kakkar LeetCode",
+    "Moaksh Kakkar CodeChef",
+    "Moaksh Kakkar Codeforces",
+    "Moaksh Kakkar HackerEarth",
+    "mokshkakkar",
+    "moakshkakkarportfolio",
+    "moakshkakkarresume",
+    "moakshkakkarblog",
+    "moakshkakkarlinkedin",
+    'moksh kakkar',
+  ]
 };
 
 export default function RootLayout({
